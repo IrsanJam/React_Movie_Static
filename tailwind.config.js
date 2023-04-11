2af4f0cc5f87eacc683692dd070c588d56495201
IrsanJam <Irsandjamaludin@gmail.com>
2023-04-11 21:46:56 +0700

    ubah data

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: "5rem",
+    },
+    extend: {
+      colors: {
+        wFont: "rgb(71 85 105)",
+        primary: "rgb(74 222 128)",
+        basecolor: "rgb(51 65 85)",
+        basefont: "rgb(203 213 225)",
+      },
+    },
   },
   plugins: [],
 };
